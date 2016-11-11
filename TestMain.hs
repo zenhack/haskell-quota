05ad09086e3d2402808d0ae6ef8fb58b3fa6f802
Ian Denhardt <ian@zenhack.net>
2016-11-10 22:52:00 -0500

    Factor out some logic in the test suite

--- TestMain.hs
+++ TestMain.hs
@@ -41,16 +41,19 @@ isConsistent (Left TraverseError, q) | traverseLimit q < 0 = True
 isConsistent (Right _, q) | recurseLimit q > 0 && traverseLimit q > 0 = True
 isConsistent _ = False
 
-propQuotaIsConsistent :: Quota -> Op -> Bool
-propQuotaIsConsistent q qa = isConsistent $ fixExn $
-    runPure (runQuotaLimitT (runOp qa) >>= pureBase) q
+
+runQuotaOp :: Quota -> Op -> (Either QuotaError (), Quota)
+runQuotaOp q op = fixExn $
+    runPure (runQuotaLimitT (runOp op) >>= pureBase) q
   where
     fixExn :: (Either SomeException a, s) -> (Either QuotaError a, s)
     fixExn (Left err, s) = (Left $ fromJust $ fromException err, s)
     fixExn (Right x, s) = (Right x, s)
 
+propIsConsistent q op = isConsistent $ runQuotaOp q op
+
 main :: IO ()
 main = defaultMain
     [ testProperty "Any sequence of quota operations is consistent."
-      propQuotaIsConsistent
+      propIsConsistent
     ]
