c8a77fe6d33d1ebc3f9cb1d46c9dff261f461740
Ian Denhardt <ian@zenhack.net>
2017-01-27 21:14:38 -0500

    Simplify
    Changes:
    
    * No language extensions
    * Shorter
    * Less tricky
    
    Per the comment at the top of Quota.hs, the state monad was more effort
    than it was worth.

--- Control/Monad/Quota.hs
+++ Control/Monad/Quota.hs
@@ -1,81 +1,58 @@
-{-|
-Module : Control.Monad.Quota
-Description : Track and control quota usage
--}
-module Control.Monad.Quota
-    ( MonadQuota(..)
-    , Quota(..)
-    , QuotaError(..)
-    , QuotaLimitT(..)
-    ) where
+module Control.Monad.Quota where
 
+-- NOTE: QuotaT basically re-implements the state monad internally. I
+-- (zenhack) have found that the code actually ends up being shorter
+-- and more straightforward this way than trying to reuse StateT.
+
+import Control.Monad.Catch (throwM, MonadThrow, Exception)
 import Control.Monad (when)
-import Control.Monad.Catch (Exception, MonadThrow, throwM)
-import Control.Monad.State (MonadState, get, put, StateT, runStateT)
-import Control.Monad.Except (ExceptT(..), runExceptT)
-import Control.Monad.Trans (MonadTrans, lift)
-import Data.Typeable (Typeable)
 
-class (Ord n, Num n, Monad m) => MonadQuota n m | m -> n where
-    recurse :: m a -> m a
-    invoice :: n -> m ()
-
--- | A quota to be imposed on a computation
-data Quota n = Quota
-    { recurseLimit :: n -- Maximum recursion depth
-    , traverseLimit :: n -- Total traversal limit
-    } deriving(Show)
-
--- | Thrown when a quota limit is exceeded.
 data QuotaError
-    = RecurseError -- ^ Recursion limit exceeded
-    | TraverseError -- ^ Total traversal limit exeeded
-    deriving(Show, Typeable)
+    = TraversalQuotaError
+    | RecursionQuotaError
+    deriving(Show, Eq)
 
 instance Exception QuotaError
 
-newtype QuotaLimitT m a =
-    QuotaLimitT { runQuotaLimitT :: m a }
-    deriving(Monad,Functor,Applicative)
+data Quota = Quota
+    { recursionLimit :: Int
+    , traversalLimit :: Int
+    } deriving(Show, Eq)
 
-instance MonadTrans QuotaLimitT where
-    lift = QuotaLimitT
+class MonadQuota m where
+    invoice :: Int -> m ()
+    recurse :: m a -> m a
 
-instance MonadState (Quota n) m => MonadState (Quota n) (QuotaLimitT m) where
-    put = lift . put
-    get = lift get
+newtype QuotaT m a = QuotaT { runQuotaT :: Quota -> m (a, Quota) }
 
-instance MonadThrow m => MonadThrow (QuotaLimitT m) where
-    throwM = lift . throwM
 
-instance (Ord n, Num n, MonadState (Quota n) m, MonadThrow m) => MonadQuota n (QuotaLimitT m) where
-    recurse cursor = do
-        Quota r t <- getQuota
-        setQuota (r - 1) (t - 1)
-        ret <- cursor
-        Quota r' t' <- getQuota
-        setQuota r t'
-        return ret
-    invoice n = do
-        Quota r t <- getQuota
-        when (n > t) $ throwM TraverseError
-        setQuota r (t - n)
+instance (Monad m) => Monad (QuotaT m) where
+    return x = QuotaT $ \q -> return (x, q)
+    QuotaT r >>= f = QuotaT $ \q -> do
+        (ret, q') <- r q
+        runQuotaT (f ret) q'
 
--- Helpers for MonadQuota (QuotaLimitT m):
+instance (Monad m, MonadThrow m) => MonadQuota (QuotaT m) where
+    invoice n = QuotaT $ \q -> do
+        let tlim = traversalLimit q
+        when (n > tlim) $ throwM TraversalQuotaError
+        return ((), q { traversalLimit = tlim - n })
+    recurse (QuotaT m) = do
+        invoice 1
+        QuotaT $ \q -> do
+            let rlim = recursionLimit q
+            when (rlim < 1) $ throwM RecursionQuotaError
+            (ret, q') <- m $ q { recursionLimit = rlim - 1 }
+            return (ret, q' { recursionLimit = rlim })
 
-getQuota :: (Ord n, Num n, MonadState (Quota n) m, MonadThrow m) => m (Quota n)
-getQuota = do
-    q@(Quota r t) <- get
-    checkQuota r t
-    return q
 
-setQuota :: (Ord n, Num n, MonadState (Quota n) m, MonadThrow m) => n -> n -> m ()
-setQuota r t = do
-    checkQuota r t
-    put $ Quota r t
-
-checkQuota :: (Ord n, Num n, MonadState (Quota n) m, MonadThrow m) => n -> n -> m ()
-checkQuota r t = do
-    when (r < 0) $ throwM RecurseError
-    when (t < 0) $ throwM TraverseError
-    return ()
+-- These are just the standard ways of derving Applicative & Functor
+-- from Monad; nothing quota-specific going on here:
+instance (Monad m) => Applicative (QuotaT m) where
+    pure = return
+    f <*> x = do
+        x' <- x
+        f' <- f
+        return (f' x')
+instance (Monad m, Applicative m) => Functor (QuotaT m) where
+    fmap f q = pure f <*> q
--- TestMain.hs
+++ TestMain.hs
@@ -1,40 +1,48 @@
 module Main where
 
-import Data.Maybe (fromJust)
 import Control.Monad.Quota
-import Control.Monad.Quota.Pure
-import Control.Monad.State (State, runState, StateT(..), runStateT, put, get)
-import Control.Monad.Catch (SomeException, fromException)
+import Control.Monad.Catch (SomeException)
 import Control.Monad.Catch.Pure (CatchT(..))
+import Data.Functor.Identity (Identity(..))
 import Test.Framework (defaultMain)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.QuickCheck.Arbitrary (Arbitrary, arbitrary)
-import Test.QuickCheck.Gen (oneof)
+import Test.QuickCheck.Gen (oneof, Gen)
 
 arbitraryNat = abs <$> arbitrary
 
-instance Arbitrary (Op Int) where
+data QuotaOp = Recurse QuotaOp
+             | Invoice Int
+             | Then QuotaOp QuotaOp
+             deriving(Show, Eq)
+
+instance Arbitrary QuotaOp where
     arbitrary = oneof [ Recurse <$> arbitrary
                       , Invoice <$> arbitraryNat
+                      , Then <$> arbitrary <*> arbitrary
                       ]
 
-instance Arbitrary (Quota Int) where
+instance Arbitrary Quota where
     arbitrary = Quota <$> arbitraryNat <*> arbitraryNat
 
-type Result = (Either QuotaError (), Quota Int)
+interpQuotaOp :: QuotaOp -> QuotaT (CatchT Identity) ()
+interpQuotaOp (Recurse qo) = recurse (interpQuotaOp qo)
+interpQuotaOp (Invoice n)  = invoice n
+interpQuotaOp (Then x y)   = interpQuotaOp x >> interpQuotaOp y
 
-runQuotaOps :: Quota Int -> [Op Int] -> Result
-runQuotaOps q ops = fixExn $ runQuotaLimit (interpOps ops) q
-  where
-    fixExn :: (Either SomeException (), Quota Int) -> Result
-    fixExn (Left err, s) = (Left $ fromJust $ fromException err, s)
-    fixExn (Right x, s) = (Right x, s)
+runPureQuota :: QuotaT (CatchT Identity) a -> Quota -> Either SomeException (a, Quota)
+runPureQuota qt q = runIdentity $ runCatchT $ runQuotaT qt q
 
-resultProp :: (Result -> Bool) -> Quota Int -> [Op Int] -> Bool
-resultProp p q ops = p (runQuotaOps q ops)
+noNegativeQuotas :: QuotaOp -> Quota -> Bool
+noNegativeQuotas qo q = case runPureQuota (interpQuotaOp qo) q of
+    Left _ -> True
+    Right ((), q')
+        | recursionLimit q' < 0 -> False
+        | traversalLimit q' < 0 -> False
+        | otherwise -> True
 
 main :: IO ()
 main = defaultMain
     [ testProperty "No sequence of operations reduces the quotas below zero."
-      (resultProp (\(_, Quota r t) -> r >= 0 && t >= 0))
+      noNegativeQuotas
     ]
