af3a2a8db308bba17e60f799cb0aaf098c799a45
Ian Denhardt <ian@zenhack.net>
2017-09-30 21:29:34 -0400

    Add a bunch of documentation.

--- src/Control/Monad/Quota.hs
+++ src/Control/Monad/Quota.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE FlexibleInstances, MultiParamTypeClasses,
     UndecidableInstances, GeneralizedNewtypeDeriving #-}
+-- | Description: Monad for tracking quota limits.
 module Control.Monad.Quota
     ( Quota(..)
     , QuotaError(..)
@@ -16,21 +17,33 @@ import Control.Monad.Trans.Class (MonadTrans(..))
 
 import Control.Monad.State (StateT, runStateT, evalStateT, MonadState(..))
 
+-- | An exception indicating that the quota was exceeded.
 data QuotaError
     = QuotaError
     deriving(Show, Eq)
 
 instance Exception QuotaError
 
+-- | A quota.
 newtype Quota = Quota Int
               deriving(Show, Eq, Num)
 
+-- | mtl-style type class for tracking quotas.
+--
+-- laws:
+--
+-- * @invoice 0 >> m = m@
 class MonadQuota m where
+    -- | @invoice n@ deducts @n@ from the quota.
     invoice :: Int -> m ()
 
+-- | Monad transformer implementing MonadQuota on top of a 'MonadThrow'.
+-- When the quota hits 0, it invokes @throwM QuotaError@.
 newtype QuotaT m a = QuotaT (StateT Quota m a)
     deriving(Functor, Applicative, Monad)
 
+-- | Run a 'QuotaT' with the given 'Quota'. Returns the monadic result
+-- of the computation and the remaining quota.
 runQuotaT :: QuotaT m a -> Quota -> m (a, Quota)
 runQuotaT (QuotaT st) = runStateT st
 
@@ -50,5 +63,7 @@ instance MonadState s m => MonadState s (QuotaT m) where
     get = lift get
     put = lift . put
 
+-- | Like 'runQuotaT', but only returns the monadic result (not the remaining
+-- quota).
 evalQuotaT :: (Monad m, Applicative m) => QuotaT m a -> Quota -> m a
 evalQuotaT (QuotaT st) q = evalStateT st q
